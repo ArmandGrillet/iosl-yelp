0674232a4ac732138a3485da7f4df6432a84c63a
ishan210788 <ishan210788@gmail.com>
2015-10-25 01:11:30 +0200

    Adding new js file for retrieving all the Restaurant Business types in a city

---
+++ queries/Types_Restaurant_in_City.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var path = require('path');
+
+
+function datasetPath(dataset) {
+    return 'dfs.`' + path.normalize(__dirname + '/../../yelp_dataset_challenge_academic_dataset/') + 'yelp_academic_dataset_' + dataset + '.json`'
+}
+
+function doQuery(query, callback) {
+    resp = {}
+    request.post(
+        'http://localhost:8047/query.json', // Address of Apache Drill
+        { 
+            json: {
+                'queryType': 'SQL',
+                'query': query
+            }
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                callback(body);
+            }
+        }
+    );
+}
+// Your code
+doQuery("select categories[0] as Categories_of_Restaurant_in_City, count(categories[0]) as Category_Count from " + datasetPath('business') + " where true=repeated_contains(categories,'Restaurants') and city='Phoenix' group by categories[0] order by count(categories[0]) desc", 
+		function(firstAnswer) {
+    console.log("List of restaurant types in a city and there counts:");
+    for (var row of firstAnswer.rows) {
+        console.log('There are ' + row.Category_Count + " "+ row.Categories_of_Restaurant_in_City +" restaurant type in Phoenix");
+    }
+}
+);
