734efc80e41e01d419001c41596a5cfbebbd5a4b
Pierre-Henri MATHIEU <PierreHenri@MacBook-Pro-de-Pierre-Henri.local>
2015-11-17 19:21:55 +0100

    'Hotzones' working (grid opacity changes according to ratio (number of checkins in a square)/(max number of checkins in a square)

--- queries/hotzones.js
+++ queries/hotzones.js
@@ -1,5 +1,4 @@
 var utils = require('./utils');
-var grid_example = require('./grid_example');
 
 function getCheckinsByBusinessId(city, callback) {
     utils.askDrill("SELECT table_checkin.business_id,table_checkin.checkin_info from " + utils.datasetPath('checkin') + " AS table_checkin INNER JOIN " + utils.datasetPath('business') + " AS table_business ON table_checkin.business_id = table_business.business_id WHERE table_business.city = '" + city + "'", function(answer) {
@@ -8,67 +7,71 @@ function getCheckinsByBusinessId(city, callback) {
     });
 }
 
-// var to be used in 2 functions so they're put here as global
 var sum = []; // number of checkins for one business
-//var min_value = 0; // minimum number of checkins per business
-//var max_value = 0; // maximum number of checkins per business
 var total_sum = 0; // total number of checkins in every business of the city
 
 module.exports = {
     get: function(parameters, callback) {
         getCheckinsByBusinessId(parameters.city, function(businesses) {
 
-            for (var init = 0; init < businesses.length; init++) {
-                sum[businesses[init].business_id] = 0;
+            for (var k = 0; k < businesses.length; k++) {
+                sum[businesses[k].business_id] = 0;
             }
+
             for (var i = 0; i < businesses.length; i++) {
                 checkin = JSON.parse(businesses[i].checkin_info);
                 for (var key in checkin) {
                     sum[businesses[i].business_id] += checkin[key];
                 }
-                //            if (sum[businesses[i].business_id] > max_value)
-                //                max_value = sum[businesses[i].business_id];
-                //            if (sum[businesses[i].business_id] < min_value)
-                //                min_value = sum[businesses[i].business_id];
                 total_sum += sum[businesses[i].business_id];
             }
 
             utils.getGrid(parameters.city, function(grid) {
                 var gridPolygons = [];
                 var ratioCheckins = []; // number of checkins per grid[][] divided by total number of checkins
-                var ratioMax = 0;
-                var ratioMin = 0;
-                var maxBusinesses = 0;
-
-                // ratioMin and ratioMax are initialized to the value of the first grid[][]
-                //for (var count = 0; count < grid[0][0].business_ids.length; count++) {
-                //    ratioMax += sum[business_ids[count]] / total_sum;
-                //}
-                //ratioMin = ratioMax;
+                var checkinMax = 0; //max number of checkins per grid[][]
 
                 // fill ratioCheckins with 0 for the += operation
                 for (i = 0; i < grid.length; i++) {
                     ratioCheckins[i] = [];
                     for (j = 0; j < grid[i].length; j++) {
                         ratioCheckins[i][j] = 0;
-                        if (maxBusinesses < grid[i][j].business_ids.length) {
-                            maxBusinesses = grid[i][j].business_ids.length;
+                        for (var p = 0; p < grid[i][j].business_ids.length; p++) {
                         }
                     }
                 }
 
-                // browsing the grid
+                /*                griddd = [];
+                                for (i = 0; i < grid.length; i++) { // 0.001° = 111.32 m
+                                    for (j = 0; j < grid[i].length; j++) {
+                                        for (var k = 0; k < grid[i][j].business_ids.length; k++) {
+                                            griddd[grid[i][j].business_ids[k]] = 0.2; // There is at least 2 businesses in the area.
+                                        }
+                                    }
+                                }
+                                console.log(Object.keys(sum).length);
+                                console.log(Object.keys(griddd).length);
+                                */
+
+                // browsing the grid & adding values
                 for (i = 0; i < grid.length; i++) { // 0.001° = 111.32 m
-                    ratioCheckins[i] = [];
                     for (j = 0; j < grid[i].length; j++) {
                         if (grid[i][j].business_ids.length >= 2) { // There is at least 2 businesses in the area.
-                            for (var k = 0; k < grid[i][j].business_ids.length; k++) {
-                                ratioCheckins[i][j] += sum[grid[i][j].business_ids[k]] / total_sum;
+                            for (k = 0; k < grid[i][j].business_ids.length; k++) {
+                                if (typeof (sum[grid[i][j].business_ids[k]]) != 'undefined') {
+                                    ratioCheckins[i][j] += sum[grid[i][j].business_ids[k]];
+                                }
                             }
-                            if (ratioCheckins[i][j] > ratioMax)
-                                ratioMax = ratioCheckins[i][j];
-                            //if (ratioCheckins[i][j] < ratioMin)
-                            //    ratioMin = ratioCheckins[i][j];
+                            if (ratioCheckins[i][j] > checkinMax)
+                                checkinMax = ratioCheckins[i][j];
+                        }
+                    }
+                }
+                // browsing the grid and adding the opacity ratio
+                for (i = 0; i < grid.length; i++) { // 0.001° = 111.32 m
+                    for (j = 0; j < grid[i].length; j++) {
+                        if (grid[i][j].business_ids.length >= 2) { // There is at least 2 businesses in the area.
+
                             gridPolygons.push({
                                 points: grid[i][j].points,
                                 popup: grid[i][j].business_ids.length.toString(),
@@ -76,10 +79,9 @@ module.exports = {
                                     stroke: false,
                                     fill: true,
                                     fillColor: "#FF0000",
-                                    fillOpacity: (grid[i][j].business_ids.length / maxBusinesses)
+                                    fillOpacity: (ratioCheckins[i][j] / checkinMax)
                                 }
                             });
-
                         }
                     }
                 }
