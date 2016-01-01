7665da60afce8cbfe62888d30042d92175a10031
Armand Grillet <armand.a.grillet@campus.tu-berlin.de>
2016-01-01 09:53:11 +0100

    Parser now dynamic (you can pass arguments to it to create the JSON file for the city you want).

--- parser/parser.js
+++ parser/parser.js
@@ -31,78 +31,83 @@ function getCenter(nodes, list) {
     return center;
 }
 
-var obj;
-var dir = '/Users/Armand/Google Drive/Master/Year 2/Semester 3/IoSL/Documents/Algorithm/Features/Sources/Edinburgh/'; // Directory with all the JSON files for a city.
+var inputDir;
+var outputFile;
 
-var goodJSON = {
-    types: []
-};
-var center;
+process.argv.forEach(function(val, index, array) {
+    if (index === 2) {
+        inputDir = val;
+        if (inputDir.slice(-1) !== '/') {
+            inputDir += '/'; // Needs to be a directory.
+        }
+    } else if (index == 3) {
+        outputFile = val;
+    }
+});
 
-fs.readdir(dir, function(err, files) {
-    if (err)
-        throw err;
-    async.each(files, function(file, callback) {
+if (inputDir === undefined || outputFile === undefined) {
+    console.log("Usage: node parser INPUTDIRECTORY OUTPUTFILE. E.g. 'node parser path/to/city/ city' will create a city.json");
+} else {
+    var obj;
+    var goodJSON = {
+        types: []
+    };
+    var center;
 
-        if (path.extname(file) == '.json') {
-            fs.readFile(dir + file, 'utf8', function(err, data) {
-                if (err)
-                    throw err;
+    fs.readdir(inputDir, function(err, files) {
+        if (err) {
+            throw err;
+        }
+        async.each(files, function(file, callback) {
+            if (path.extname(file) == '.json') {
+                fs.readFile(inputDir + file, 'utf8', function(err, data) {
+                    if (err)
+                        throw err;
 
-                var name = file.substring(file.indexOf('_') + 1);
-                name = name.replace('_edinburgh.json', '');
-                obj = JSON.parse(data);
-                for (var i = 0; i < obj.elements.length; i++) {
-                    if (i === 0) {
-                        goodJSON.types.push(name);
-                        goodJSON[name] = [];
-                    }
-
-                    if (name === 'zoo') {
-                        console.log(obj.elements[i].type);
-                        if (obj.elements[i].type === 'way') {
-                            console.log(obj.elements[i].nodes);
+                    var name = file.substring(file.indexOf('_') + 1);
+                    name = name.replace(/_[A-Za-z0-9_]+.json/, '');
+                    obj = JSON.parse(data);
+                    for (var i = 0; i < obj.elements.length; i++) {
+                        if (i === 0) {
+                            goodJSON.types.push(name);
+                            goodJSON[name] = [];
                         }
-                    }
 
-                    if (obj.elements[i].tags !== undefined) { // A real feature
-                        if (obj.elements[i].type == 'node') { // Simple coordinates
-                            goodJSON[name].push({
-                                lat: obj.elements[i].lat,
-                                lon: obj.elements[i].lon
-                            });
-                        } else if (obj.elements[i].type === 'way') {
-                            if (name === 'zoo') {
-                                console.log(obj.elements[i]);
-                            }
-                            goodJSON[name].push(getCenter(obj.elements[i].nodes, obj.elements)); // Multiple coordinates, we need to compute the center
-                        } else if (obj.elements[i].type === 'relation') {
-                            for (var j = 0; j < obj.elements[i].members.length; j++) { // Add tags to the main elements of the relation so that we'll add them later.
-                                for (var k = 0; k < obj.elements.length; k++) {
-                                    if (obj.elements[k].id === obj.elements[i].members[j].ref) {
-                                        obj.elements[k].tags = obj.elements[i].tags;
+                        if (obj.elements[i].tags !== undefined) { // A real feature
+                            if (obj.elements[i].type == 'node') { // Simple coordinates
+                                goodJSON[name].push({
+                                    lat: obj.elements[i].lat,
+                                    lon: obj.elements[i].lon
+                                });
+                            } else if (obj.elements[i].type === 'way') {
+                                goodJSON[name].push(getCenter(obj.elements[i].nodes, obj.elements)); // Multiple coordinates, we need to compute the center
+                            } else if (obj.elements[i].type === 'relation') {
+                                for (var j = 0; j < obj.elements[i].members.length; j++) { // Add tags to the main elements of the relation so that we'll add them later.
+                                    for (var k = 0; k < obj.elements.length; k++) {
+                                        if (obj.elements[k].id === obj.elements[i].members[j].ref) {
+                                            obj.elements[k].tags = obj.elements[i].tags;
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                }
+                    callback();
+                });
+            } else {
                 callback();
-            });
-        } else {
-            callback();
-        }
-    }, function(err) {
-        if (err) {
-            console.log('A file failed to process.');
-        } else {
-            fs.writeFile("./edinburgh.json", JSON.stringify(goodJSON), function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log("The file was saved!");
-            });
-        }
+            }
+        }, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                fs.writeFile("./" + outputFile + ".json", JSON.stringify(goodJSON), function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log(outputFile + " was saved!");
+                });
+            }
+        });
     });
-});
+}
