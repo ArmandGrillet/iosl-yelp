c137be304f762905dd561f110baf37f08dd8111c
Armand Grillet <armand.a.grillet@campus.tu-berlin.de>
2016-02-07 10:24:36 +0100

    Refactoring of Ishan's code.

--- queries/hotspots.js
+++ queries/hotspots.js
@@ -1,145 +1,84 @@
+/* Show the hotspots in a city for a specified category */
+
 var utils = require('./utils'); // Functions used by multiple queries are in utils.
 
-function getHostpots(hotspots) { //Function to make the array with unique values
-    var uniqueStringHotspots = [];
-    var uniqueHotspots = [];
-    for (var i = 0; i < hotspots.length; i++) {
-        if (uniqueStringHotspots.indexOf(JSON.stringify(hotspots[i])) == -1) {
-            uniqueStringHotspots.push(JSON.stringify(hotspots[i]));
-            uniqueHotspots.push(hotspots[i]);
-        }
+// Return one point from multiple points.
+function centerPoint(points) {
+    var meanLatitude = 0;
+    var meanLongitude = 0;
+
+    for (var i = 0; i < points.length; i++) {
+        meanLatitude = parseFloat(meanLatitude) + parseFloat(points[i].latitude);
+        meanLongitude = parseFloat(meanLongitude) + parseFloat(points[i].longitude);
     }
+    meanLatitude = meanLatitude / points.length;
+    meanLongitude = meanLongitude / points.length;
 
-    return uniqueHotspots;
+    return {
+        'latitude': meanLatitude.toString(),
+        'longitude': meanLongitude.toString()
+    };
 }
 
-function get_hotspots(longitude, latitude) {
-    var relative_distance; //Relative distance from other Restaurants
-    var hotspot_count_value = 10;
-    var hotspots = [];
-    var temp_hotspots = [];
-    var hotspot_cluster_shop_list = [];
-    var hotspot_cluster_mean_point_list = [];
-    var hotspot_count = 0; //Hotspot count to count the number of shops around for which we have chosen 5, so if cluster has more than 5 shops it's an Hotspot region
-    for (var i = 0; i < longitude.length; i++) { //loop to iterate through the list of latitudes and longitudes and calculate distance between them
-        hotspot_count = 0;
-        for (var j = 0; j < longitude.length; j++) {
-            relative_distance = utils.distance(latitude[i], longitude[i], latitude[j], longitude[j]); // calculating distance in KMs between two points on map
-            if (relative_distance < 3 && relative_distance > 0) { //We have taken distance 1.5 KM if the shops are neighbouring
-                hotspot_count++;
-                temp_hotspots.push({
-                    'latitude': latitude[j],
-                    'longitude': longitude[j]
-                });
-                if (hotspot_count > hotspot_count_value) { //capturing the coordinates when it is considered a hotspot cluster, '=' because we just want to add once
-                    hotspot_cluster_shop_list.push({ //Adding it to Cluster shop list
-                        'latitude': latitude[j],
-                        'longitude': longitude[j]
-                    });
-                    hotspots.push({
-                        'latitude': latitude[j],
-                        'longitude': longitude[j]
-                    });
-                }
-            }
-        }
-        if (hotspot_count > hotspot_count_value) {
-            for (j = 0; j < temp_hotspots.length; j++) {
-                hotspots.push({
-                    'latitude': temp_hotspots[j].latitude,
-                    'longitude': temp_hotspots[j].longitude
-                });
-                hotspot_cluster_shop_list.push({ // Adding it to Cluster shop list
-                    'latitude': temp_hotspots[j].latitude,
-                    'longitude': temp_hotspots[j].longitude
-                });
-            }
-        } else {
-            hotspot_cluster_shop_list = [];
-        }
-        temp_hotspots = [];
-        if (hotspot_count > hotspot_count_value) { //capturing the coordinates of base shop from where we were calculating distances from other shops
-            hotspots.push({
-                'latitude': latitude[i],
-                'longitude': longitude[i]
-            });
-            hotspot_cluster_shop_list.push({
-                'latitude': latitude[i],
-                'longitude': longitude[i]
-            });
-            hotspot_cluster_shop_list = getHostpots(hotspot_cluster_shop_list);
-        }
-        if (hotspot_count > hotspot_count_value) { //Here we are adding the all the shops in the cluster list and sending them to the function to get the mean of there cluster
-            var temp_coordinates = [];
-            temp_coordinates = meanHotspotPoint(hotspot_cluster_shop_list);
-            for (j = 0; j < temp_coordinates.length; j++) {
-                hotspot_cluster_mean_point_list.push({
-                    'latitude': temp_coordinates[j].latitude,
-                    'longitude': temp_coordinates[j].longitude
-                });
-            }
-        }
-        hotspot_cluster_shop_list = [];
-    }
-    return getHostpots(meanHotspotsList(hotspot_cluster_mean_point_list)); // returning mean list of all clusters with unique value
-    //return getHostpots(hotspots);
-}
-
-function meanHotspotsList(hotspot_cluster_mean_point_list) {
-    return hotspot_cluster_mean_point_list;
-}
-
-function meanHotspotPoint(hotspot_cluster_shop_list) {
-    var mean_latitude = 0;
-    var mean_longitude = 0;
-    var mean_hotspot_point = [];
-    var no_of_shops = hotspot_cluster_shop_list.length;
-    for (var i = 0; i < hotspot_cluster_shop_list.length; i++) {
-        mean_latitude = parseFloat(mean_latitude) + parseFloat(hotspot_cluster_shop_list[i].latitude);
-        mean_longitude = parseFloat(mean_longitude) + parseFloat(hotspot_cluster_shop_list[i].longitude);
-    }
-    mean_latitude = mean_latitude / no_of_shops;
-    mean_longitude = mean_longitude / no_of_shops;
-
-    mean_hotspot_point.push({
-        'latitude': mean_latitude,
-        'longitude': mean_longitude
-    });
-    return mean_hotspot_point;
-}
-
-function list_of_shops_in_a_category(city, business, callback) {
-    utils.askDrill("select categories[0] as Categories_of_Restaurant_in_Phoenix, latitude, longitude from " + utils.datasetPath('business') + " where city='" + city + "' and categories[0]='" + business + "'", function(answer) {
-        var longitude = [];
-        var latitude = [];
-        var count = 0;
+// Return all the businesses in a category.
+function getBusinessesInCategory(city, category, callback) {
+    utils.askDrill("select latitude, longitude from " + utils.datasetPath('business') + " where city='" + city + "' and true=repeated_contains(categories,'" + category + "')", function(answer) {
+        var businesses = [];
         for (var i = 0; i < answer.rows.length; i++) {
-            count++;
-            longitude[count] = answer.rows[i].longitude;
-            latitude[count] = answer.rows[i].latitude;
+            businesses.push({
+                latitude: answer.rows[i].latitude,
+                longitude: answer.rows[i].longitude
+            });
         }
-        callback(get_hotspots(longitude, latitude));
+        callback(businesses);
     });
+}
+
+// Find businesses with more than 10 similar businesses in a radius of 3KM.
+function getHotspots(businesses) {
+    var distance; // Relative distance from other Restaurants
+    var numberOfBusinessesForHotspot = 10;
+    var hotspots = [];
+    var businessesInHotspot = [];
+
+    // For each businesses we're getting the number of businesses in a radius of 500m, if there are more than 10 competitors it is a hotspot.
+    for (var i = 0; i < businesses.length; i++) {
+        businessesNearby = 0;
+        businessesInHotspot = [];
+        for (var j = 0; j < businesses.length; j++) {
+            distance = utils.distance(businesses[i].latitude, businesses[i].longitude, businesses[j].latitude, businesses[j].longitude) * 1000;
+            if (distance < 500) { // The distance needs to be inferior to 500 meters to be a "cluster"
+                businessesInHotspot.push(businesses[j]);
+            }
+        }
+        if (businessesInHotspot.length > numberOfBusinessesForHotspot) {
+            hotspots.push(businesses[i]);
+            hotspots.push(centerPoint(businessesInHotspot));
+        }
+    }
+    return hotspots;
 }
 
 module.exports = {
     get: function(parameters, callback) {
-        if (parameters.category === undefined) { // We only need to test name as city is a mandatory attribute {
+        // We need the category of the business for this algorithm.
+        if (parameters.category === undefined) {
             callback({
                 error: 'Parameter category is undefined'
             });
         } else {
-            list_of_shops_in_a_category(parameters.city, parameters.category, function(hotspot) {
+            // Get all the businesses in the category.
+            getBusinessesInCategory(parameters.city, parameters.category, function(businesses) {
+                var hotspots = getHotspots(businesses); // Get the hotspots using the above algorithm.
+                // The answer will be circles representing the hotspots.
                 var answer = {
-                    circles: [],
-                    markers: []
+                    circles: []
                 };
-                for (var i = 0; i < hotspot.length; i++) {
+                for (var i = 0; i < hotspots.length; i++) {
                     answer.circles.push({
-                        latitude: hotspot[i].latitude,
-                        longitude: hotspot[i].longitude,
-                        radius: 500,
-                        popup: "Hostspot for " + parameters.category,
+                        latitude: hotspots[i].latitude,
+                        longitude: hotspots[i].longitude,
+                        radius: 200,
                         options: {
                             stroke: false,
                             fillColor: "#FF0000"
@@ -151,8 +90,9 @@ module.exports = {
         }
     },
     test: function() {
-        list_of_shops_in_a_category('Phoenix', 'Fast Food', function(hotspot) {
-            console.log("Result of the algorithm: " + hotspot.length);
+        getBusinessesInCategory('Phoenix', 'Fast Food', function(businesses) {
+            var hotspots = getHotspots(businesses);
+            console.log("Result of the algorithm: " + hotspots.length);
         });
     }
 };
--- queries/hotzones.js
+++ queries/hotzones.js
@@ -1,4 +1,5 @@
 /* Display the hot zones of a city using the number of checkins in a tile */
+
 var utils = require('./utils'); // Functions used by multiple queries are in utils.
 
 function getBusinessesWithCheckins(city, callback) {
@@ -12,22 +13,25 @@ module.exports = {
         getBusinessesWithCheckins(parameters.city, function(businesses) {
             var i, j;
             var checkin;
-            var checkinsPerBusiness = {}; // number of checkins for one business
+            var checkinsPerBusiness = {}; // Number of checkins for one business
             for (i = 0; i < businesses.length; i++) {
                 checkinsPerBusiness[businesses[i].business_id] = 0;
                 checkin = JSON.parse(businesses[i].checkin_info);
+                // The checkin variable is a JSON object containing the number of checkins at a specific time, we only need the number of checkins per day.
                 for (var key in checkin) {
                     checkinsPerBusiness[businesses[i].business_id] += checkin[key];
                 }
             }
 
             utils.getGrid(parameters.city, function(grid) {
+                // We'll send to Leaflet only polygons representing tiles.
                 var answer = {
                     polygons: []
                 };
-                var maxCheckins = 0; // Maximum number of checkins in the grid.
 
+                var maxCheckins = 0; // Maximum number of checkins in the grid.
                 for (i = 0; i < grid.features.length; i++) {
+                    // We had a new value to the grid JSON called checkins, representing the number of checkins in a tile.
                     grid.features[i].properties.checkins = 0;
                     for (j = 0; j < grid.features[i].properties.business_ids.length; j++) {
                         if (checkinsPerBusiness[grid.features[i].properties.business_ids[j]] !== undefined) {
@@ -39,8 +43,8 @@ module.exports = {
                     }
                 }
 
-                for (i = 0; i < grid.features.length; i++) {
-                    if (grid.features[i].properties.business_ids.length >= 2) { // There is at least 2 businesses in the area.
+                for (i = 0; i < grid.features.length; i++) { // For each tile
+                    if (grid.features[i].properties.business_ids.length >= 2) { // There is at least 2 businesses in the tile.
                         answer.polygons.push({
                             points: [
                                 {
@@ -60,17 +64,17 @@ module.exports = {
                                     longitude: grid.features[i].geometry.coordinates[3][1]
                                 }
                             ],
-                            popup: grid.features[i].properties.checkins.toString(),
+                            popup: grid.features[i].properties.checkins.toString(), // The popup is the number of checkins.
                             options: {
                                 stroke: false,
                                 fill: true,
                                 fillColor: "#FF0000",
-                                fillOpacity: (grid.features[i].properties.checkins / maxCheckins)
+                                fillOpacity: (grid.features[i].properties.checkins / maxCheckins) // The opacity depends on the number of checkins.
                             }
                         });
                     }
                 }
-
+                // Send back the tiles.
                 callback(answer);
             });
         });
